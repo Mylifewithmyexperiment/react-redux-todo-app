5665579f51594327cebca948fb9e0ae880d78553
SHASHI <shashi.jaiswal17@gmail.com>
2020-08-21 21:18:36 +0530

    source folders

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import App from './App';
+import TodoApp from "./TodoApp";
 import * as serviceWorker from './serviceWorker';
 
+//called only once 
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  rootElement
 );
+ 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
