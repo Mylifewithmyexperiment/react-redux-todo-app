5fdf71c1f950d2a7e725e4f5e55f27e2ed70b4cc
SHASHI <shashi.jaiswal17@gmail.com>
2020-08-21 20:06:00 +0530

    Updates

---
+++ precache-manifest.81e693eaa18ee5dc2d4b1389dc95c465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a1f17937f1671933215e50bdf8f146",
+    "url": "/Mylifewithmyexperiment/react-redux-todo-app/index.html"
+  },
+  {
+    "revision": "657f077a84c06b730c76",
+    "url": "/Mylifewithmyexperiment/react-redux-todo-app/static/css/main.55d2dd05.chunk.css"
+  },
+  {
+    "revision": "526bf37d38c2df7b718c",
+    "url": "/Mylifewithmyexperiment/react-redux-todo-app/static/js/2.497bb966.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Mylifewithmyexperiment/react-redux-todo-app/static/js/2.497bb966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657f077a84c06b730c76",
+    "url": "/Mylifewithmyexperiment/react-redux-todo-app/static/js/main.31403274.chunk.js"
+  },
+  {
+    "revision": "9700ec1eda760702a9cb",
+    "url": "/Mylifewithmyexperiment/react-redux-todo-app/static/js/runtime-main.4bbce7bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mylifewithmyexperiment/react-redux-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
